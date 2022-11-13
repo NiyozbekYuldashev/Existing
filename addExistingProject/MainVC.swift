b7ecb6a5fc41a8fb48616027970f1ff585123eb4
NiyozbekYuldashev <yniyozbek@gmail.com>
2022-11-13 13:40:01 +0500

    mainVC add

---
+++ addExistingProject/MainVC.swift
@@ -0,0 +1,17 @@
+//
+//  MainVC.swift
+//  addExistingProject
+//
+//  Created by Niyozbek Yuldashev on 13/11/22.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
