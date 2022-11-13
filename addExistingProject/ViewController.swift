8cfa7344c969bf2a7698c1a07110d66ce958a586
NiyozbekYuldashev <yniyozbek@gmail.com>
2022-11-13 13:38:41 +0500

    red

--- addExistingProject/ViewController.swift
+++ addExistingProject/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpImg()
+        self.view.backgroundColor = .red
     }
     func setUpImg() {
         
